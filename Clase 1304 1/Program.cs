dc40dd40eb7c6b78a216593de683ef99da1325de
Starlogic - Nicolás Fumo <starlogicsoftware@gmail.com>
2024-04-13 19:54:43 -0300

    Subido clase 13/04
    Codigos y diagrama de clases del día 13/04/2024

---
+++ Clase 1304 1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+//int resultado = 23;
+//string Texto = $"Esto es un texto con este ---: {resultado}";
+
+//string textoCambiado = Texto.Replace("---", "resultado");
+
+//Console.WriteLine(textoCambiado);
+
+
+
+// fechas y horas
+DateTime fecha = new DateTime(2009,5,21, 21,15,35);
+DateTime fechaActual = DateTime.Now.AddDays(-21);
+
+//DateTime fechaModificada =  fechaActual.AddDays(-21);
+
+//fechaActual = fechaModificada;
+
+fechaActual = fechaActual.AddDays(-21);
+
+//Console.WriteLine(fechaActual.ToShortDateString()); // fecha sola
+//Console.WriteLine(fechaActual.ToShortTimeString()); // hora sola
+//Console.WriteLine(fechaActual.ToLongDateString()); // fecha de forma larga
+//Console.WriteLine(fechaActual.ToLongTimeString()); // hora de forma larga
+
+//Console.WriteLine(fechaActual.ToString("dddd yyyy MM FF")); // hora de forma larga
+
+TimeSpan lapsoTiempo = fechaActual - fecha; // lapso de tiempo entre dos fechas
+
+TimeSpan lapsoNuevo = new TimeSpan(2, 5, 40); //lapso creado a mano
+
+Console.WriteLine(lapsoTiempo.TotalDays); // cantidad dias entre dos fechas
---
+++ Clase 1304 2/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+Console.WriteLine("Ingrese un numero");
+
+try
+{
+    int divisor = int.Parse(Console.ReadLine());
+
+    if(divisor == 0)
+    {
+        // Se dispara una excepcion "personalizada"
+        throw new Exception("Trataste de hacer una division por cero, no sea tonto");
+    }
+
+    int resultado = 20 / divisor;
+    Console.WriteLine($"Resultado: {resultado}");
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("Lo que ingresaste no es un numero valido");
+}
+catch(DivideByZeroException ex)
+{
+    Console.WriteLine("Trataste de dividir por cero");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Este codigo deberia de ejecutarse siempre!");
+}

d8d88395031419887f3c219073ae9a5cff017ef3
Starlogic - Nicolás Fumo <starlogicsoftware@gmail.com>
2024-05-04 19:42:02 -0300

    Fin clase 04/05

---
+++ EstacionamientoMedido/Vistas/Menu.cs
@@ -0,0 +1,48 @@
+﻿using EstacionamientoMedido.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstacionamientoMedido.Vistas
+{
+    public class Menu
+    {
+        ClienteVista vistaCliente = new ClienteVista();
+
+        public void MostrarMenu()
+        {
+            int eleccion;
+            Console.WriteLine("1- Cargar un cliente");
+            Console.WriteLine("2- Ver clientes registrados");
+            Console.WriteLine();
+            Console.Write("Opcion: ");
+            eleccion = int.Parse(Console.ReadLine());
+
+            switch (eleccion)
+            {
+                case 1:
+
+                    vistaCliente.CargarDatosCliente();
+
+                    Console.WriteLine(); 
+                    MostrarMenu(); 
+
+                    break;
+                case 2:
+
+                    vistaCliente.MostrarClientesRegistrados();
+
+                    Console.WriteLine();
+                    MostrarMenu();
+                    break;
+
+                default:
+                    Console.Clear();
+                    MostrarMenu();
+                    break;
+            }
+        }
+    }
+}
---
+++ Genericos/Program.cs
@@ -0,0 +1,58 @@
+﻿
+int dato1, dato2;
+
+GestorRespuesta respuestaMetodo;
+
+Console.Write("Primer numero: ");
+dato1 = int.Parse(Console.ReadLine());
+
+Console.Write("Segundo numero: ");
+dato2 = int.Parse(Console.ReadLine());
+
+respuestaMetodo = Dividir(dato1, dato2);
+
+if (respuestaMetodo.HayError)
+{
+    Console.WriteLine($"El resultado es: {respuestaMetodo.Error}");
+}
+else
+{
+    Console.WriteLine($"El resultado es: {respuestaMetodo.Resultado}");
+}
+
+GestorRespuesta Dividir(int a, int b)
+{
+    if (b == 0)
+    {
+        return new GestorRespuesta<int,string>(true, "No se peude divir por cero");
+    }
+    else if (b < 0)
+    {
+        return new GestorRespuesta<string,string>(true, "No se puede divir por un valor menor que cero");
+    }
+    else
+    {
+        int resultado = a / b;
+        return new GestorRespuesta<int,string>(false, resultado);
+    }
+}
+
+class GestorRespuesta<Tipo,T2>
+{
+
+    public Tipo Resultado;
+    public T2 Error;
+    public bool HayError;
+    public GestorRespuesta(bool hayError, T2 mensajeError)
+    {
+        HayError = hayError;
+        Error = mensajeError;
+    }
+
+    public GestorRespuesta(bool hayError, Tipo resultado)
+    {
+        HayError = hayError;
+        Resultado = resultado;
+    }
+
+}

ef188bd00e3d66818ee772f525c888edaa76c29f
Starlogic - Nicolás Fumo <starlogicsoftware@gmail.com>
2024-05-18 19:34:12 -0300

    Final clase 18/05

---
+++ Interfaces/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+IRepositorio repo = new RepositorioVehiculos();
+
+class RepositorioVehiculos : IRepositorio
+{
+    public void Guardar()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Obtener(string patente)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OtroMetodo()
+    {
+
+    }
+}
+
+class RepositorioString : IRepositorio, IBaseDatos
+{
+    public void AccederDB()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Guardar()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Obtener(string patente)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class Vehiculo { }
+
+interface IRepositorio
+{
+    void Obtener(string patente);
+
+    void Guardar();
+}
+
+interface IBaseDatos
+{
+    void AccederDB();
+}
